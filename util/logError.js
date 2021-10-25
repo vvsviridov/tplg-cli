e4d97763a567b85b44a5a145df1f8938194a2ce4
Vyacheslav.Sviridov <atx@inbox.ru>
2021-10-25 13:06:55 +0600

    forgotten awaits

--- util/logError.js
+++ util/logError.js
@@ -7,7 +7,9 @@ function logError(err) {
   } else if (err.response && err.response.data && err.response.data.title) {
     console.log(`${err.response.data.title.red.bold}: ${err.response.data.body.yellow}`)
     console.log(`Error Code ${err.response.data.errorCode.toString().yellow}: ${err.response.data.detail.gray}`)
-  } else {
+  } else if (err.response && err.response.status && err.response.data.statusText) {
+    console.log(`${err.response.status}: ${err.response.statusText}`.red)
+  }else {
     console.error(err)
   }  
 }
