6d87b6e061d6e5fed99c5db3772de404956a3ebc
Vyacheslav.Sviridov <atx@inbox.ru>
2021-10-29 11:12:30 +0600

    axiosConfig

--- util/requestWrapper.js
+++ util/requestWrapper.js
@@ -2,12 +2,28 @@ const ora = require('ora')
 const logError = require('./logError')
 
 
-async function requestWrapper(func, text = 'Reading Topology...') {
+// async function requestWrapper(func, text = 'Reading Topology...') {
+//   const spinner = ora(text)
+//   let result
+//   try {
+//     spinner.start()
+//     result = await func()
+//     spinner.succeed()
+//   } catch (err) {
+//     result = err.response
+//     spinner.fail()
+//     logError(err)
+//   } finally {
+//     return result
+//   }
+// }
+
+async function requestWrapper(axiosInstance, axiosConfig, text = 'Reading Topology...') {
   const spinner = ora(text)
   let result
   try {
     spinner.start()
-    result = await func()
+    result = await axiosInstance.request(axiosConfig)
     spinner.succeed()
   } catch (err) {
     result = err.response
