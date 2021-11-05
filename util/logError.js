e1436bdff5e6650cb07a9a7e3498b782a0d5be2b
Vyacheslav.Sviridov <atx@inbox.ru>
2021-11-05 13:27:02 +0600

    minor errors

--- util/logError.js
+++ util/logError.js
@@ -3,11 +3,11 @@ const colors = require('colors')
 
 function logError(err) {
   if (err.response && err.response.data && err.response.data.message){
-    console.error(`${err.response.status}: ${err.response.statusText}, ${err.response.data.message}`.red)
+    console.log(`${err.response.status}: ${err.response.statusText}, ${err.response.data.message}`.red)
   } else if (err.response && err.response.data && err.response.data.title) {
     console.log(`${err.response.data.title.red.bold}: ${err.response.data.body.yellow}`)
     console.log(`Error Code ${err.response.data.errorCode.toString().yellow}: ${err.response.data.detail.gray}`)
-  } else if (err.response && err.response.status && err.response.data.statusText) {
+  } else if (err.response && err.response.status && err.response.statusText) {
     console.log(`${err.response.status}: ${err.response.statusText}`.red)
   }else {
     console.error(err)
